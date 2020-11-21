c09dd0c237ce24344bd32be37c34dbcb3befac50
mariammartinez <mariammartinez@users.noreply.github.com>
2020-11-21 18:40:52 +0100

    animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,24 @@
 "use strict";
 
 require("../css/app.scss");
-require("jquery");
+const $ = require("jquery");
 require("bootstrap");
 
 
+$(function() {
+	/**
+    * Smooth scrolling to page anchor on click
+    **/
+	$("a[href*='#']:not([href='#'])").click(function() {
+		if (
+			location.hostname == this.hostname
+            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+		) {
+			let anchor = $(this.hash);
+			anchor = anchor.length ? anchor : $(`[name=${  this.hash.slice(1) }]`);
+			if ( anchor.length ) {
+				$("html, body").animate( { scrollTop: anchor.offset().top }, 1500);
+			}
+		}
+	});
+});
